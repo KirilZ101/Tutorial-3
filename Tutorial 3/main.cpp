ac981474c473d68d8f7319acd5cb0c7e8c09688f
KirilZ101 <152603999+KirilZ101@users.noreply.github.com>
2024-02-11 11:21:31 +0300

    Initial Commit

---
+++ Tutorial 3/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Tutorial 3
+//
+//  Created by Кирилл Заболотний on 11.02.2024.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
